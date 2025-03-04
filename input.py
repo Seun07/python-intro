0112c3a8423df50fe5c7d725c26866186caa2c61
“Seun07” <macbook@MacBooks-MacBook-Air.local>
2025-03-04 21:32:17 +0100

    my initial commit

---
+++ input.py
@@ -0,0 +1,6 @@
+a=100
+b=500
+c=(a+b)-200
+print("This is the final result",c)
+d=c*20
+print("final result",d)
---
+++ multiply.py
@@ -0,0 +1,7 @@
+a=31
+b=41
+c=51
+d=a*b*c
+
+print("this is the result of multiplication",d)
+
