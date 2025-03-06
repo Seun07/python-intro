61549485ccaba63d0ed5e8c218b80bb037e8f551
“Seun07” <macbook@MacBooks-MacBook-Air.local>
2025-03-06 20:41:14 +0100

    my initial commit

--- userinput1.py
+++ userinput1.py
@@ -1,5 +1,8 @@
-print("Enter your fist name")
-firstname=input()
-print("Enter your surname")
-surname=input()
-print("Details" , firstname ,surname) 
+print("Enter school name")
+schoolname=input()
+print("Enter your school email")
+schoolemail=input()
+print("Enter school phone number")
+schoolphonenumber=input()
+print("details" ,schoolname ,schoolemail ,schoolphonenumber)
+
